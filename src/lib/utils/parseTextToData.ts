f1ad40ed91f866a877ba11a4bf977ad66b2d7e49
toki23 <blancshio@gmail.com>
2022-02-04 01:52:25 +0900

    適当にグラフ出した

--- src/lib/utils/parseTextToData.ts
+++ src/lib/utils/parseTextToData.ts
@@ -1,3 +1,14 @@
-export default ()=>{
+export type xyData =  {
+    x:number[],
+    y:number[]
+}
+
+export default (text:string):xyData=>{
+
+    let a:xyData = {
+        x:[1,2,3,4],
+        y:[1,4,9,16]
+    };
+    return a 
 
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,45 @@
 <script>
+	import '@carbon/charts/styles.min.css';
+	import 'carbon-components/css/carbon-components.min.css';
+
+	import parseTextTodata from '$lib/utils/parseTextToData';
+	import { LineChart } from '@carbon/charts-svelte';
+
 	let data = '';
 	const inputeData = () => {
-		console.log(data);
+		console.log(parseTextTodata(data));
 	};
 </script>
 
 <h1>input data here</h1>
 <textarea bind:value={data} on:input={inputeData} />
+
+<LineChart
+	data={[
+		{ group: 1, key: 1, value: 1 },
+		{ group: 1, key: 2, value: 4 },
+		{ group: 1, key: 3, value: 9 },
+		{ group: 1, key: 4, value: 16 },
+		{ group: 1, key: 5, value: 25 }
+	]}
+	options={{
+		title: 'Line (discrete)',
+		axes: {
+			bottom: {
+				title: '2019 Annual Sales Figures',
+				mapsTo: 'key',
+				scaleType: 'labels'
+			},
+			left: {
+				mapsTo: 'value',
+				title: 'Conversion rate',
+				scaleType: 'linear'
+			}
+		},
+		height: '400px'
+	}}
+/>
+
 <!-- graph Data
 1	2
 2	4
