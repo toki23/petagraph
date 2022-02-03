5e60cf94c42ed80c971bd8580880358d30d1827c
toki23 <blancshio@gmail.com>
2022-02-04 01:58:50 +0900

    グラフのコンポーネント化

---
+++ src/lib/components/chart.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '@carbon/charts/styles.min.css';
+	import 'carbon-components/css/carbon-components.min.css';
+	import { LineChart } from '@carbon/charts-svelte';
+</script>
+<LineChart
+	data={[
+		{ group: 1, key: 1, value: 1 },
+		{ group: 1, key: 2, value: 4 },
+		{ group: 1, key: 3, value: 9 },
+		{ group: 1, key: 4, value: 16 },
+		{ group: 1, key: 5, value: 25 }
+	]}
+	options={{
+		title: 'Line (discrete)',
+		axes: {
+			bottom: {
+				title: '2019 Annual Sales Figures',
+				mapsTo: 'key',
+				scaleType: 'labels'
+			},
+			left: {
+				mapsTo: 'value',
+				title: 'Conversion rate',
+				scaleType: 'linear'
+			}
+		},
+		height: '400px'
+	}}
+/>
